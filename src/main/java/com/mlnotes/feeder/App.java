dcf956392189ae065bc269b9545ebc2864483765
Hanfeng Zhu <me@mlnotes.com>
2014-09-25 11:35:18 +0800

    push data to queue without data controlling

---
+++ src/main/java/com/mlnotes/feeder/App.java
@@ -0,0 +1,32 @@
+package com.mlnotes.feeder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import redis.clients.jedis.Jedis;
+
+/**
+ *
+ * @author Zhu Hanfeng <me@mlnotes.com>
+ */
+public class App {
+    private final Jedis jedis = new Jedis(Constants.REDIS_SERVER);
+    
+    // TODO how to control feeding rate
+    public void pushData() throws IOException{
+        InputStream is = this.getClass().getResourceAsStream(Constants.DATA);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        String line;
+        int count = 0;
+        while((line = reader.readLine()) != null && count < 100){
+            jedis.rpush(Constants.QUEUE_NAME, line);
+            count++;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        App app = new App();
+        app.pushData();
+    }
+}
---
+++ src/main/java/com/mlnotes/feeder/Constants.java
@@ -0,0 +1,12 @@
+package com.mlnotes.feeder;
+
+/**
+ *
+ * @author Zhu Hanfeng <me@mlnotes.com>
+ */
+public class Constants {
+    public static String REDIS_SERVER = "storm5.mlnotes.com";
+    public static String QUEUE_NAME = "weibo";
+    
+    public static String DATA = "/data";
+}
